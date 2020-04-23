53c5ec66e23edee73aaed14e6b019b3638619a08
George Williams <george412williams@gmail.com>
2020-04-23 15:53:41 -0500

    add tests and ability to say hello to any input

--- code.js
+++ code.js
@@ -4,11 +4,12 @@ function helloWorld() {
 }
 
 function sayHello(input) {
-    if (input === "Pat") {
-        return "Hello, Pat!";
-    } else if (input === "Alex") {
-        return "Hello, Alex!";
-    } else {
-        return "Hello, Jane!";
-    }
+    return "Hello, " + input + "!";
+    // if (input === "Pat") {
+    //     return "Hello, Pat!";
+    // } else if (input === "Alex") {
+    //     return "Hello, Alex!";
+    // } else {
+    //     return "Hello, Jane!";
+    // }
 }
